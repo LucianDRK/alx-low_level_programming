4ccf47d4c86008ecddf5ccd834db07ee85663fd5
LucianDRK <jakephilander57@gmail.com>
2022-10-14 04:48:55 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,7 @@
  * main - Entry point
  * Return: Always 0
  */
-int main(void);
-{
-	puts("\"Programming is like building a multilingual puzzle\n");
+int main(void)
+{	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
