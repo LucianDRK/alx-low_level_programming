394c071a41870f2d5f26209ddf20ce3bee69be13
LucianDRK <jakephilander57@gmail.com>
2022-10-14 19:59:58 -0700

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,13 @@ int main(void)
 	int a;
 
 	for (a = 0; a < 100; a++)
-		printf(a + '0');
+	{	printf(a + '0');
 		if (a < 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
+	}
 	putchar('\n');
 	return (0);
 }
