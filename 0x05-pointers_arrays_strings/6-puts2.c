6d2564136aacd170bfaf0dcf637c03d6f76ccf53
LucianDRK <jakephilander57@gmail.com>
2022-10-25 15:02:41 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one character out of two
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int p = 0;
+
+	for (*str[p] != '\0'; p++)
+	{
+		if ((p % 2) == 0)
+			_putchar(*str[p]);
+		else
+			continue;
+	}
+	_putchar('\n');
+}
