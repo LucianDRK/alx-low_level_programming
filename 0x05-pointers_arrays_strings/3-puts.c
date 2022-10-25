98e64f2fd24307dff264fc8361ffecc8cb873c73
LucianDRK <jakephilander57@gmail.com>
2022-10-25 14:22:30 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string to be printed
+ */
+void _puts(char *str)
+{
+	char *str;
+
+	str = "what happened here";
+
+	_putchar(str);
+
+	_putchar("\n");
+
+		return (0);
+}
