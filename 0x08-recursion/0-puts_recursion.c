6831e8645cb40273a648abf5bb9a6341413d00c7
LucianDRK <jakephilander57@gmail.com>
2022-11-02 14:05:01 -0700

    taso 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: char to be printed
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(" ", '\n');
+	return (0);
+}
