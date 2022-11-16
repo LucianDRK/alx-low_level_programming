719223dd6a0b58a991424a027d02a352770af582
LucianDRK <jakephilander57@gmail.com>
2022-11-16 12:53:56 -0800

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,7 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog dog_t
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct *d);
 dog_t *new_dog(char *name, float age, char *owner);
